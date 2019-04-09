d46ea0112b2a9e929418b9a315c0496a3f1b9710
Christopher Robinson <CRobinson@student.neumont.edu>
2019-04-09 15:14:10 -0600

    Add Air Hockey Project Prefix to All Air Hockey Assets

--- Interactive Board/Assets/Scripts/AH_Goal.cs
+++ Interactive Board/Assets/Scripts/AH_Goal.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Goal : MonoBehaviour
+public class AH_Goal : MonoBehaviour
 {
     [SerializeField]
     bool IsRightGoal;
 
-    private GameMaster gm;
+    private AH_GameMaster gm;
 
     private void Start()
     {
-        gm = FindObjectOfType<GameMaster>();   
+        gm = FindObjectOfType<AH_GameMaster>();   
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.GetComponent<Puck>() != null)
+        if(collision.gameObject.GetComponent<AH_Puck>() != null)
         {
             gm.GivePointToPlayer(IsRightGoal);
             Destroy(collision.gameObject, 2.0f);
--- Interactive Board/Assets/Scripts/AH_Puck.cs
+++ Interactive Board/Assets/Scripts/AH_Puck.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Puck : MonoBehaviour
+public class AH_Puck : MonoBehaviour
 {
     [SerializeField]
     float m_maxSpeed;
