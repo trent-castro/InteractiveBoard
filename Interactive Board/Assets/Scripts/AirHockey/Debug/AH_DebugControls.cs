88a3a0ea00752283518bb32efb4f5acda9cec5e1
Sen <keima.ayasaki@gmail.com>
2019-04-09 16:37:35 -0600

    Puck updates for debug and removal of instantiation.

---
+++ Interactive Board/Assets/Scripts/AirHockey/Debug/AH_DebugControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AH_DebugControls : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+    public Vector2 velocity;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rigidBody.velocity += velocity;
+    }
+}
---
+++ Interactive Board/Assets/Scripts/AirHockey/PowerUps/AH_PowerUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AH_PowerUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Puck"))
+        {
+            CauseEffect(collision.gameObject.GetComponent<AH_Puck>());
+        }
+    }
+
+    /// <summary>
+    /// Disables the power up and returns the position back to the parent transform
+    /// </summary>
+    public void DisablePowerUp()
+    {
+        gameObject.SetActive(false);
+        transform.position = transform.parent.position;
+    }
+
+    public virtual void CauseEffect(AH_Puck puck) { }
+}
