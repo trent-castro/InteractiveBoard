949375f49f4d1e97e692c86807507a650fc070da
Christopher Robinson <CRobinson@student.neumont.edu>
2019-04-15 16:33:51 -0600

    limit touches to capture area

--- Interactive Board/Assets/Scripts/Common/MultiTouchManager.cs
+++ Interactive Board/Assets/Scripts/Common/MultiTouchManager.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,6 +19,7 @@ public class MultiTouchManager : MonoBehaviour
         }
     }
 
+    public Collider2D m_captureArea = null;
     public TouchInfo[] currentTouches;
 
     private Dictionary<int, TouchInfo> m_touches { get; set; } = new Dictionary<int, TouchInfo>();
@@ -55,9 +56,20 @@ public class MultiTouchManager : MonoBehaviour
 
     void Update()
     {
-        foreach (Touch touch in Input.touches)
+        Debug.Log("Touches length" + Input.touches.Length);
+        Debug.Log("Touch info length" + m_touches.Count);
+        foreach (Touch t in Input.touches)
         {
-            if (touch.phase != TouchPhase.Began && touches.ContainsKey(touch.fingerId))
+            Touch touch = t;
+
+            if (m_captureArea != null)
+            {
+                if (!m_captureArea.OverlapPoint(Camera.main.ScreenToWorldPoint(touch.position)))
+                {
+                    touch.phase = TouchPhase.Ended;
+                }
+            }
+
             if (touch.phase != TouchPhase.Began && m_touches.ContainsKey(touch.fingerId))
             {
                 m_touches[touch.fingerId].UpdateTouchInfo(touch);
@@ -70,7 +82,6 @@ public class MultiTouchManager : MonoBehaviour
                 {
                     HandleCurrentTouches?.Invoke(m_touches[touch.fingerId]);
                 }
-
             }
             else if (!m_touches.ContainsKey(touch.fingerId))
             {
@@ -80,7 +91,6 @@ public class MultiTouchManager : MonoBehaviour
 
                 HandleNewTouches?.Invoke(newTouch);
             }
-
         }
     }
 }
