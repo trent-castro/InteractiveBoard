7842fca17e7feeda3440f6d4d9fc2a29f1eee304
lmckamey <lucas.mckamey1@gmail.com>
2019-04-16 14:49:54 -0600

    MetaData and Invisible Fix

--- Interactive Board/Assets/Scripts/AirHockey/PickUps/Actual PickUps/AH_PU_Invisible.cs
+++ Interactive Board/Assets/Scripts/AirHockey/PickUps/Actual PickUps/AH_PU_Invisible.cs
@@ -6,27 +6,11 @@ public class AH_PU_Invisible : AH_PickUp
 {
     public override void OnEffectBegin()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        afflictedPuck.GetComponentInChildren<SpriteRenderer>().enabled = false;
-=======
-        afflictedPuck.GetComponent<SpriteRenderer>().enabled = false;
->>>>>>> parent of e0f8210... Merge branch 'master' into Lmckamey-ParticleSystems
-=======
-        afflictedPuck.GetComponent<SpriteRenderer>().enabled = false;
->>>>>>> parent of e0f8210... Merge branch 'master' into Lmckamey-ParticleSystems
+       afflictedPuck.GetComponentInChildren<SpriteRenderer>().enabled = false;
     }
 
     public override void OnEffectEnd()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
         afflictedPuck.GetComponentInChildren<SpriteRenderer>().enabled = true;
-=======
-        afflictedPuck.GetComponent<SpriteRenderer>().enabled = true;
->>>>>>> parent of e0f8210... Merge branch 'master' into Lmckamey-ParticleSystems
-=======
-        afflictedPuck.GetComponent<SpriteRenderer>().enabled = true;
->>>>>>> parent of e0f8210... Merge branch 'master' into Lmckamey-ParticleSystems
     }
 }
