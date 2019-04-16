0943b8fa6b7d25ab23ec9fdcf0def3223e1de506
lmckamey <lucas.mckamey1@gmail.com>
2019-04-16 14:29:04 -0600

    Update AH_PU_Invisible.cs

--- Interactive Board/Assets/Scripts/AirHockey/PickUps/Actual PickUps/AH_PU_Invisible.cs
+++ Interactive Board/Assets/Scripts/AirHockey/PickUps/Actual PickUps/AH_PU_Invisible.cs
@@ -6,7 +6,7 @@ public class AH_PU_Invisible : AH_PickUp
 {
     public override void OnEffectBegin()
     {
-        afflictedPuck.GetComponent<SpriteRenderer>().enabled = false;
+        afflictedPuck.GetComponentInChildren<SpriteRenderer>().enabled = false;
     }
 
     public override void OnEffectEnd()
