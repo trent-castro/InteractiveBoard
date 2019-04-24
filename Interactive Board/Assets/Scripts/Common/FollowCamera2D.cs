b5fff13379b4d2b05cf72f8da5ddb22328fbf767
Christopher Robinson <CRobinson@student.neumont.edu>
2019-04-23 23:31:17 -0600

    Control, Camera, and Movement Changes

--- Interactive Board/Assets/Scripts/Common/FollowCamera2D.cs
+++ Interactive Board/Assets/Scripts/Common/FollowCamera2D.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class FollowCamera2D : MonoBehaviour
 {
-    public Transform m_target = null;
-    private Vector3 m_velocity = new Vector3();
+    public Rigidbody2D m_target = null;
+    private Vector2 m_velocity = Vector2.zero;
+    private Vector2 m_targetOffset = Vector2.zero;
 
     // Update is called once per frame
     void FixedUpdate()
     {
         if (m_target == null) return;
 
-        Vector3 targetPosition = new Vector3(m_target.transform.position.x, m_target.transform.position.y, transform.position.z);
+        Vector2 targetPosition = new Vector2(m_target.transform.position.x, m_target.transform.position.y);
+        m_targetOffset = Vector2.Lerp(m_targetOffset, m_target.velocity * 0.3f + (Vector2)m_target.transform.up * 1.5f, Time.deltaTime * 5);
 
-        Vector3 oldPosition = transform.position;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref m_velocity, .05f);
+        Vector2 oldPosition = transform.position;
+        transform.position = Vector2.SmoothDamp(transform.position, targetPosition + m_targetOffset, ref m_velocity, .15f);
+        transform.position += Vector3.forward * -10;
     }
 }
