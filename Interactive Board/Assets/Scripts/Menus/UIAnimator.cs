3b5408d172e0f567c276b53d810bd4deb6fae81f
Unknown <nenschan@gmail.com>
2019-04-25 13:05:13 -0600

    Auto stash before merge of "MN-Menuing" and "master"

---
+++ Interactive Board/Assets/Scripts/Menus/UIAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAnimator : MonoBehaviour
+{
+	[SerializeField] Vector3 m_startLocation;
+	[SerializeField] Vector3 m_endLocation;
+	[SerializeField] private float m_timer;
+	private float m_time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
