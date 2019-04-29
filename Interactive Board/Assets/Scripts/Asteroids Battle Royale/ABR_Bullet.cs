2128a32efb02a87fecd30e30f85c81af9df45ba4
lmckamey <lucas.mckamey1@gmail.com>
2019-04-29 14:49:22 -0600

    Bullet and Turrets added

---
+++ Interactive Board/Assets/Scripts/Asteroids Battle Royale/ABR_Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ABR_Bullet : MonoBehaviour
+{
+    [SerializeField] float lifeTime = 10.0f;
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] bool DebugMode = false;
+
+    private Vector2 movementDirection = Vector2.zero;
+    void Update()
+    {
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0.0f)
+            gameObject.SetActive(false);
+
+        gameObject.transform.position += (Vector3)(movementDirection * speed);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //TODO put damage and or hit detection logic here
+        gameObject.SetActive(false);
+    }
+
+    public void Fire(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+}
