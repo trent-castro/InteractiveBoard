7a232e7b48098557ad485b89dfb374c025cffffb
lmckamey <lucas.mckamey1@gmail.com>
2019-05-01 13:57:40 -0600

    Weapon Beginnings

---
+++ Interactive Board/Assets/Scripts/Asteroids Battle Royale/Weapon/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eBulletType
+{
+    BASIC,
+    EXPLOSION,
+    PIERCE,
+    LASER
+}
+
+public abstract class Weapon
+{
+    float m_fireDelay;
+    string m_bulletTye;
+    uint m_ammo;
+    abstract public void Fire();
+}
+
+public class Shotgun : Weapon
+{
+    public Shotgun()
+    {
+
+    }
+
+    public override void Fire()
+    {
+      
+    }
+}
