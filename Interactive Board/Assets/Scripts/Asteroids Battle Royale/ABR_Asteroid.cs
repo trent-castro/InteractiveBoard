a9a7615657fd3c1112a724b57ff77856fdcbfcd1
Unknown <nenschan@gmail.com>
2019-05-02 14:11:19 -0600

    More asteroids; Camera box UI color coded and added ui for joining players

--- Interactive Board/Assets/Scripts/Asteroids Battle Royale/ABR_Asteroid.cs
+++ Interactive Board/Assets/Scripts/Asteroids Battle Royale/ABR_Asteroid.cs
@@ -7,14 +7,14 @@ public class ABR_Asteroid : MonoBehaviour
 {
 	const float c_maxXDirection = 130;
 	const float c_maxYDirection = 48;
-	[SerializeField] Vector2 m_startLocation;
-	[SerializeField] Vector2 m_endLocation;
-	[SerializeField] Vector2 velocity;
-	[SerializeField] Camera boy;
-	ParticleSystem m_shards;
-	Rigidbody2D m_rb;
+	[SerializeField] Vector2 m_startLocation = Vector2.zero;
+	[SerializeField] Vector2 m_endLocation = Vector2.zero;
+	[SerializeField] Vector2 velocity = Vector2.zero;
+	[SerializeField] Camera boy = null;
+	ParticleSystem m_shards = null;
+	Rigidbody2D m_rb = null;
 	float speed = 3;
-	[Range(0, 100)] int health;
+	[Range(0, 100)] int health = 100;
 
 	private void Start()
 	{
@@ -25,6 +25,29 @@ public class ABR_Asteroid : MonoBehaviour
 		velocity = m_rb.velocity;
 		transform.position = m_startLocation;
 		m_shards = GetComponentInChildren<ParticleSystem>();
+	}
+
+	/// <summary>
+	/// Sets a target and start location for the object to be moved between via rigidbody velocity.
+	/// </summary>
+	/// <param name="currentLocation">The starting location of the object.</param>
+	/// <param name="targetLocation">The intended end location of the object.</param>
+	public void Target(Vector2 currentLocation, Vector2 targetLocation)
+	{
+		//Clamps Target to playable area
+		currentLocation = new Vector2(Mathf.Clamp(currentLocation.x, -c_maxXDirection, c_maxXDirection), Mathf.Clamp(currentLocation.y, -c_maxYDirection, c_maxYDirection));
+		targetLocation = new Vector2(Mathf.Clamp(targetLocation.x, -c_maxXDirection, c_maxXDirection), Mathf.Clamp(targetLocation.y, -c_maxYDirection, c_maxYDirection));
+
+		m_startLocation = currentLocation;
+		m_endLocation = targetLocation;
+
+		//Sets velocity as a linear path between the start and end locations
+		m_rb.velocity = (m_endLocation - m_startLocation) * speed;
+		m_rb.velocity = new Vector2(Mathf.Clamp(m_rb.velocity.x, -10, 10), Mathf.Clamp(m_rb.velocity.y, -10, 10));
+
+		//Resets other factors of object
+		m_rb.angularVelocity = 0;
+		transform.position = m_startLocation;
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision)
@@ -49,12 +72,10 @@ public class ABR_Asteroid : MonoBehaviour
 	{
 		if (collision.gameObject.tag == "Wall")
 		{
-			//m_startLocation = new Vector2(Random.Range(-c_maxXDirection, c_maxXDirection), c_maxYDirection);
 			m_startLocation = new Vector2(Mathf.Clamp(boy.ScreenToWorldPoint(Input.mousePosition).x, -c_maxXDirection, c_maxXDirection), c_maxYDirection);
-			//m_endLocation = new Vector2(Random.Range(-c_maxXDirection, c_maxXDirection), -c_maxYDirection);
-			m_endLocation = new Vector2(boy.ScreenToWorldPoint(Input.mousePosition).x, -c_maxYDirection);
+			m_endLocation   = new Vector2(Mathf.Clamp(boy.ScreenToWorldPoint(Input.mousePosition).x, -c_maxXDirection, c_maxXDirection), -c_maxYDirection);
 			m_rb.angularVelocity = 0;
-			m_rb.velocity = (m_startLocation - m_endLocation) * speed;
+			m_rb.velocity = ( m_endLocation - m_startLocation) * speed;
 			m_rb.velocity = new Vector2(Mathf.Clamp(m_rb.velocity.x, -10, 10), Mathf.Clamp(m_rb.velocity.y, -10, 10));
 			transform.position = m_startLocation;
 		}
