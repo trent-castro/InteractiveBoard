9a9cf889d631841c458cbb559c55c28201ebcd4a
Unknown <nenschan@gmail.com>
2019-05-03 14:01:50 -0600

    Reworking air hockey menu

--- Interactive Board/Assets/Scripts/Air Hockey/UI/AH_SetUpMenu.cs
+++ Interactive Board/Assets/Scripts/Air Hockey/UI/AH_SetUpMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Assets.Scripts.AirHockey.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 /// <summary>
@@ -11,9 +12,16 @@ using UnityEngine.SceneManagement;
 // marked for fixing
 public class AH_SetUpMenu : MonoBehaviour
 {
+	static public AH_SetUpMenu instance;
 	static public ETheme defaultTheme = ETheme.BASIC;
 	[SerializeField] Sprite[] m_themeBackgrounds = null;
 	[SerializeField] Image m_background = null;
+	[SerializeField] Sprite[] m_themeForegrounds = null;
+	[SerializeField] Image m_foreground = null;
+
+	[SerializeField] TextMeshProUGUI m_pointsRequiredText = null;
+	[Range(3, 7)] int m_pointsRequired = 3;
+
 	ETheme m_theme;
 
 	int m_player1 = 0;
@@ -22,6 +30,7 @@ public class AH_SetUpMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+		instance = this;
 		SetUpTheme(defaultTheme);
 
     }
@@ -31,9 +40,14 @@ public class AH_SetUpMenu : MonoBehaviour
 		m_theme = newTheme;
 
 		m_background.sprite = m_themeBackgrounds[(int)newTheme];
+		m_foreground.sprite = m_themeForegrounds[(int)newTheme];
 		
 	}
 
+	public ETheme GetCurrentTheme()
+	{
+		return m_theme;
+	}
 
 	public void ChoosePuck(int choice)
 	{
@@ -57,6 +71,15 @@ public class AH_SetUpMenu : MonoBehaviour
 		PlayerPrefs.SetInt("AirHockeyTheme", (int)m_theme);
 		PlayerPrefs.SetInt("AirHockeyPlayer1Puck", m_player1);
 		PlayerPrefs.SetInt("AirHockeyPlayer2Puck", m_player2);
+	}
 
+	public void SetPointsRequired(int offset)
+	{
+		m_pointsRequired += offset;
+		if (m_pointsRequired < 3) m_pointsRequired = 3;
+		if (m_pointsRequired > 7) m_pointsRequired = 7;
+
+		m_pointsRequiredText.text = m_pointsRequired.ToString();
+		PlayerPrefs.SetInt("AirHockeyPointsRequired", m_pointsRequired);
 	}
 }
