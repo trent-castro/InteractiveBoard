028f4e3c103a8dbdf5ae52401b063f8cc68eb6ad
Unknown <nenschan@gmail.com>
2019-05-03 15:22:50 -0600

    Added timed reset back to map to menus

--- Interactive Board/Assets/Scripts/Menus/ArcadeMenuManager.cs
+++ Interactive Board/Assets/Scripts/Menus/ArcadeMenuManager.cs
@@ -9,9 +9,30 @@ public class ArcadeMenuManager : MonoBehaviour
 	[SerializeField] List<Menu> m_Menus = null;
 	[SerializeField] string m_currentMenu = null;
 	[SerializeField] string m_launchMenu= null;
+
+	[SerializeField] GameObject m_timeWarning = null;
+
+	[SerializeField] float m_timer = 30;
+	[SerializeField] float m_time = 0;
+	bool HasOpenedTab = false;
 	private void Start()
 	{
 		ChangeMenu(m_launchMenu);
+		Time.timeScale = 1.0f;
+	}
+
+	void Update()
+	{
+		m_time += Time.unscaledDeltaTime;
+		if (m_time >= m_timer / 2)
+		{
+			TurnOnWarning();
+		}
+		if (m_time >= m_timer && !HasOpenedTab)
+		{
+			HasOpenedTab = true;
+			TimedReturnToMap();
+		}
 	}
 
 	public void ChangeMenu(string menu)
@@ -31,13 +52,22 @@ public class ArcadeMenuManager : MonoBehaviour
 
 	public void ChangeScenes(string scene)
 	{
-		{
-			SceneManager.LoadScene(scene);
-		}
-		//else
-		//{
-		//	Debug.Log("Your scene is either not loaded in Build Settings, or you can't spell. Please check " + 
-		//		scene +"(scene) in MenuManager.ChangesScenes on " + this.gameObject.name);
-		//}
+		SceneManager.LoadScene(scene);
+	}
+
+	public void ResetMapTimer()
+	{
+		m_time = 0;
+		HasOpenedTab = false;
+	}
+
+	private void TurnOnWarning()
+	{
+		m_timeWarning.SetActive(true);
+	}
+
+	private void TimedReturnToMap()
+	{
+		Application.OpenURL("https://interactive.neumont.edu/");
 	}
 }
