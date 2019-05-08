e1d0e014153b5b9efa11f7136efc1045c336b1ba
Unknown <nenschan@gmail.com>
2019-05-08 14:44:40 -0600

    I'm sorry sen

---
+++ Interactive Board/Assets/Scripts/Menus/MyUVScroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class MyUVScroll : MonoBehaviour {
+
+    public int materialIndex = 0;
+    public Vector2 uvAnimationRate = new Vector2(1.0f, 0.0f);
+    public string textureName = "_MainTex";
+    private Renderer myRenderer;
+    private Vector2 uvOffset = Vector2.zero;
+
+    private void Awake()
+    {
+        myRenderer = GetComponent<Renderer>();
+    }
+
+    private void LateUpdate()
+    {
+        uvOffset += (uvAnimationRate * Time.deltaTime);
+        if (myRenderer.enabled)
+        {
+            myRenderer.materials[materialIndex].SetTextureOffset(textureName, uvOffset);
+        }
+    }
+}
---
+++ Interactive Board/Assets/Scripts/Menus/UIScaleTransition.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class UIScaleTransition : MonoBehaviour
+{
+	[SerializeField] Vector3 m_endScale = Vector3.zero;
+	[SerializeField] float m_timer = 0;
+	[SerializeField] float m_timeOffset = 0;
+	[SerializeField] float m_timeVariance = 2;
+	RectTransform m_transform = null;
+	Vector3 m_startScale = Vector3.zero;
+	float m_time = 0;
+	float m_timerRandom = 0;
+	IEnumerator c_Transition = null;
+
+	Vector3 m_start = Vector3.zero;
+	Vector3 m_end = Vector3.zero;
+	bool m_reverse = false;
+
+	// Start is called before the first frame update
+	void Awake()
+	{
+		m_transform = GetComponent<RectTransform>();
+		m_startScale = m_transform.localScale;
+		m_start = m_startScale;
+		m_end = m_endScale;
+	}
+
+	private void OnEnable()
+	{
+		StartTransformation();
+		//StartCoroutine("StartTransformation");
+	}
+	public void Reverse()
+	{
+		m_end = m_startScale;
+		m_start = m_endScale;
+		m_time = 0;
+		m_reverse = true;
+		c_Transition = UpdateOnFixedInterval();
+		StartCoroutine(c_Transition);
+	}
+	public void StartTransformation()
+	{
+		m_timerRandom = Random.Range(m_timer - m_timeVariance, m_timer);
+		m_time = 0 - m_timeOffset;
+		m_reverse = false;
+		m_transform.localScale = m_startScale;
+		m_start = m_startScale;
+		m_end = m_endScale;
+		c_Transition = UpdateOnFixedInterval();
+		StartCoroutine(c_Transition);
+	}
+
+	public IEnumerator UpdateOnFixedInterval()
+	{
+		while (m_time < m_timer)
+		{
+			m_time += Time.unscaledDeltaTime;
+			float t = m_time / m_timer;
+			t = Interpolation.ExpoInOut(t);
+			m_transform.localScale = Vector3.LerpUnclamped(m_start, m_end, t);
+			yield return null;
+		}
+		c_Transition = null;
+		if (!m_reverse)Reverse();
+	}
+}
