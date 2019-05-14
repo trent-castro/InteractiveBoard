8133207d731cda00df0762583547a65758ea5ba4
lmckamey <lucas.mckamey1@gmail.com>
2019-05-14 15:04:03 -0600

    Moved Explosion Effect | Removed debug logs

--- Interactive Board/Assets/Scripts/Asteroids Battle Royale/Bullets/ABR_BasicBullet.cs
+++ Interactive Board/Assets/Scripts/Asteroids Battle Royale/Bullets/ABR_BasicBullet.cs
@@ -12,10 +12,8 @@ public class ABR_BasicBullet : ABR_Bullet
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision);
         if(!collision.CompareTag("Wall") && ! collision.CompareTag("TouchArea"))
         {
-            Debug.Log("Collided with something else");
         }
     }
 }
