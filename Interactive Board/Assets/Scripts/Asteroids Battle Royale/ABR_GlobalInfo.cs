f8c502c4580a6d87c1833d147d1d744bccb006f0
lmckamey <lucas.mckamey1@gmail.com>
2019-05-15 14:34:00 -0600

    Pool stuff

--- Interactive Board/Assets/Scripts/Asteroids Battle Royale/ABR_GlobalInfo.cs
+++ Interactive Board/Assets/Scripts/Asteroids Battle Royale/ABR_GlobalInfo.cs
@@ -6,11 +6,16 @@ public class ABR_GlobalInfo : MonoBehaviour
 {
     [SerializeField]
     ObjectPoolManager m_bulletManager = null;
+    [SerializeField]
+    ObjectPoolManager m_weaponPickupManager = null;
+
 
     public static ObjectPoolManager BulletManager;
+    public static ObjectPoolManager WeaponPickupManager;
 
     private void Awake()
     {
         BulletManager = m_bulletManager;
+        WeaponPickupManager = m_weaponPickupManager;
     }
 }
