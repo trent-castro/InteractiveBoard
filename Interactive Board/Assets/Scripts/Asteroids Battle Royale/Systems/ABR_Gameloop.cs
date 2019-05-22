30d4ff5833f9e193df5d83de93e8015333cf4221
Sen <keima.ayasaki@gmail.com>
2019-05-22 15:23:13 -0600

    no message

--- Interactive Board/Assets/Scripts/Asteroids Battle Royale/Systems/ABR_Gameloop.cs
+++ Interactive Board/Assets/Scripts/Asteroids Battle Royale/Systems/ABR_Gameloop.cs
@@ -4,20 +4,6 @@ using UnityEngine;
 
 public class ABR_Gameloop : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
-=======
     public static ABR_Gameloop instance;
     [Header("External References")]
     [Tooltip("A reference to the player spawner.")]
@@ -25,12 +11,11 @@ public class ABR_Gameloop : MonoBehaviour
     private ABR_PlayerSpawner m_playerSpawner;
 
     // Private data members
-    private Player[] m_players;    
+    private Player[] m_players;
 
     void Awake()
     {
         instance = this;
-        m_players = m_playerSpawner.GetListOfPlayers();
     }
 
     public void CheckGameState()
@@ -44,4 +29,4 @@ public class ABR_Gameloop : MonoBehaviour
         // if multiple, tie
     }
 }
->>>>>>> cffd521fb0e101fc6479686606f2e90268433363
+}
