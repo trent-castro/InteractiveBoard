e343966519bb3d2487cca5a99728923a549fc68f
Christopher Robinson <CRobinson@student.neumont.edu>
2019-05-29 16:39:42 -0600

    Start of Reload Visual

---
+++ Interactive Board/Assets/Scripts/Asteroids Battle Royale/ABR_FireButtonDisabler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ABR_FireButtonDisabler : MonoBehaviour
+{
+    [SerializeField]
+    private ABR_Turret m_turret = null;
+
+    [SerializeField]
+    private Button m_fireButton = null;
+
+    [SerializeField]
+    private GameObject m_reloadVisual = null;
+
+    private void Update()
+    {
+
+        if (m_turret.IsOkayToFire && !m_fireButton.interactable)
+        {
+            m_reloadVisual.gameObject.SetActive(false);
+        }
+        else if (!m_turret.IsOkayToFire)
+        {
+            if (m_fireButton.interactable)
+            {
+                m_reloadVisual.gameObject.SetActive(true);
+            }
+
+            m_reloadVisual.transform.localScale = Vector3.one * Mathf.Lerp(0, 1, Interpolation.QuadraticOut(m_turret.getReloadPercent()));
+        }
+
+        m_fireButton.interactable = m_turret.IsOkayToFire;
+    }
+}
