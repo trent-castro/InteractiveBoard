259f9150bbd416483f4ed2f12425f53906d21151
Sen <keima.ayasaki@gmail.com>
2019-05-30 13:11:57 -0600

    force field

--- Interactive Board/Assets/Scripts/Asteroids Battle Royale/Health/ABR_Health.cs
+++ Interactive Board/Assets/Scripts/Asteroids Battle Royale/Health/ABR_Health.cs
@@ -13,6 +13,9 @@ public abstract class ABR_Health : MonoBehaviour
     [Tooltip("Invincibility duration delay")]
     [SerializeField]
     private float m_invincibilityDurationDelay = 0.5f;
+    [Tooltip("Invincibility Sprite")]
+    [SerializeField]
+    private GameObject m_invincibilitySprite = null;
 
     protected bool isInvincible = false;
     protected bool invincibilityCoroutineHasStarted = false;
@@ -20,6 +23,7 @@ public abstract class ABR_Health : MonoBehaviour
 	public delegate void DeathAction();
 	public event DeathAction OnDeath;
 
+    private Vector3 invincibiliySpriteNormalScale = new Vector3();
 
     public float GetHealth()
 	{
@@ -36,6 +40,10 @@ public abstract class ABR_Health : MonoBehaviour
 		m_health = health;
 		m_maxHealth = maxHealth;
 		m_isAlive = true;
+        if(m_invincibilitySprite)
+        {
+            invincibiliySpriteNormalScale = m_invincibilitySprite.transform.localScale;
+        }
 	}
 
 	public virtual void Respawn()
@@ -70,11 +78,48 @@ public abstract class ABR_Health : MonoBehaviour
         if (!invincibilityCoroutineHasStarted)
         {
             invincibilityCoroutineHasStarted = true;
+            // start coronary heart exam here
             yield return new WaitForSeconds(m_invincibilityDurationDelay);
             isInvincible = true;
+            if (m_invincibilitySprite)
+            {
+                m_invincibilitySprite.SetActive(true);
+                //StartCoroutine(ProjectForceField());
+            }
             yield return new WaitForSeconds(m_invincibilityDuration);
             isInvincible = false;
             invincibilityCoroutineHasStarted = false;
+            if (m_invincibilitySprite)
+            {
+                m_invincibilitySprite.SetActive(false);
+                //StartCoroutine(RetractForceField());
+            }
+        }
+    }
+
+    protected IEnumerator ProjectForceField()
+    {
+        float timer = 0;
+        while(timer <= m_invincibilityDurationDelay)
+        {
+            timer += Time.deltaTime;
+            float t = timer / m_invincibilityDurationDelay;
+            t = Interpolation.Linear(t);
+            m_invincibilitySprite.transform.localScale = Vector3.LerpUnclamped(Vector3.zero, invincibiliySpriteNormalScale, t);
+            yield return null;
+        }
+    }
+
+    protected IEnumerator RetractForceField()
+    {
+        float timer = 0;
+        while (timer <= 0.5f)
+        {
+            timer += Time.deltaTime;
+            float t = timer / 0.5f;
+            t = Interpolation.Linear(t); 
+            m_invincibilitySprite.transform.localScale = Vector3.LerpUnclamped(invincibiliySpriteNormalScale, Vector3.zero, t);
+            yield return null;
         }
     }
 }
