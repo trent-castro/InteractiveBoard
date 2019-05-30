64b8e638cd540593cecbe8cbbaf58037d163d98e
Christopher Robinson <CRobinson@student.neumont.edu>
2019-05-30 17:52:57 -0600

    Fixed control changer
    It's messy, maybe disable when using certain controls

---
+++ Interactive Board/Assets/Scripts/Common/FlipPanel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipPanel : MonoBehaviour
+{
+    public void Flip()
+    {
+        transform.localScale = transform.localScale * (new Vector2(-1, 1));
+    }
+}
