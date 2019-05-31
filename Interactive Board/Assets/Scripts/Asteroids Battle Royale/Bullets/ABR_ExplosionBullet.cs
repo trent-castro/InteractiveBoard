b0449653eacb348132b7ccf9f875c0141079e051
lmckamey <lucas.mckamey1@gmail.com>
2019-05-31 15:37:18 -0600

    Explosion Bullet fixes and Image Fade

--- Interactive Board/Assets/Scripts/Asteroids Battle Royale/Bullets/ABR_ExplosionBullet.cs
+++ Interactive Board/Assets/Scripts/Asteroids Battle Royale/Bullets/ABR_ExplosionBullet.cs
@@ -5,13 +5,10 @@ using UnityEngine;
 public class ABR_ExplosionBullet : ABR_Bullet
 {
     [Header("Explosion Bullet Configuration")]
-    [SerializeField]
-    GameObject m_explosionObjects = null;
-    [SerializeField]
-    GameObject m_bulletObject = null;
-
-    public float m_explosionTime;
-
+    [SerializeField] GameObject m_explosionObjects = null;
+    [SerializeField] GameObject m_bulletObject = null;
+    [SerializeField] float m_explosionRaduis = 1.5f;
+    [SerializeField] float m_explosionForceModifier = 1.5f;
 
     private bool isExploding = false;
     public override void Fire(Vector2 direction, Vector2 shipVelocity)
@@ -43,16 +40,33 @@ public class ABR_ExplosionBullet : ABR_Bullet
         m_bulletObject.SetActive(false);
         m_explosionObjects.SetActive(true);
         isExploding = true;
+
+        Collider2D[] collisions = Physics2D.OverlapCircleAll(transform.position, m_explosionRaduis);
+        foreach (var collision in collisions)
+        {
+            ABR_Health health = collision.GetComponent<ABR_Health>();
+            if (health)
+            {
+                DealDamage(ref health);
+            }
+            Rigidbody2D rigidbody = collision.GetComponent<Rigidbody2D>();
+            if (rigidbody)
+            {
+                Vector2 directionVec = (collision.transform.position - transform.position).normalized;
+                rigidbody.AddForce(directionVec * m_explosionForceModifier);
+            }
+        }
+        
     }
 
     private new void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.gameObject.CompareTag(ABR_Tags.ShipCollisionTag) && !collision.CompareTag(ABR_Tags.WallTag) 
+        if (!collision.gameObject.CompareTag(ABR_Tags.ShipCollisionTag) && !collision.CompareTag(ABR_Tags.WallTag)
             && !collision.CompareTag(ABR_Tags.GravityTag) && !collision.CompareTag(ABR_Tags.WeaponTag))
         {
             if (!isExploding)
                 Explode();
         }
-	}
+    }
 
 }
