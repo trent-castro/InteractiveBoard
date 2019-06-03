ff6ab38b5b28ec493fe7d60f04da8cdaf62413db
Christopher Robinson <CRobinson@student.neumont.edu>
2019-06-03 15:48:50 -0600

    Continuous Fire

---
+++ Interactive Board/Assets/Scripts/Common/HoldButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HoldButton : Button, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField]
+    private UnityEvent onDown = new UnityEvent();
+    [SerializeField]
+    private UnityEvent onUp = new UnityEvent();
+
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        onDown?.Invoke();
+
+        base.OnPointerDown(eventData);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        onUp?.Invoke();
+
+        base.OnPointerUp(eventData);
+    }
+}
