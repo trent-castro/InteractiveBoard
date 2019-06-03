24cd06b44a346707721fed02d0d51be74a15227b
Sen <keima.ayasaki@gmail.com>
2019-06-03 15:38:52 -0600

    Updated ABR_GlobalInfo.cs and ABR_LeaveManager.cs

--- Interactive Board/Assets/Scripts/Asteroids Battle Royale/ABR_GlobalInfo.cs
+++ Interactive Board/Assets/Scripts/Asteroids Battle Royale/ABR_GlobalInfo.cs
@@ -5,21 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class ABR_GlobalInfo : MonoBehaviour
 {
+    [Header("External Reference")]
+    [Tooltip("An external reference to the bullet manager's object pool manager script.")]
     [SerializeField]
     ObjectPoolManager m_bulletManager = null;
+    [Tooltip("An external reference to the weapon pick up manager's object pool manager script.")]
     [SerializeField]
     ObjectPoolManager m_weaponPickupManager = null;
-
-
+    
+    /// <summary>
+    /// A public static reference to the bullet manager's object pool manager.
+    /// </summary>
     public static ObjectPoolManager BulletManager;
+    /// <summary>
+    /// A public static reference to the weapon pick up manager's object pool manager.
+    /// </summary>
     public static ObjectPoolManager WeaponPickupManager;
 
+    /// <summary>
+    /// Sets up manager references for use globally.
+    /// </summary>
     private void Awake()
     {
         BulletManager = m_bulletManager;
         WeaponPickupManager = m_weaponPickupManager;
     }
 
+    /// <summary>
+    /// Returns the user to the arcade menu.
+    /// </summary>
 	public void ReturntoAracde()
 	{
 		Time.timeScale = 1.0f;
--- Interactive Board/Assets/Scripts/Asteroids Battle Royale/ABR_LeaveManager.cs
+++ Interactive Board/Assets/Scripts/Asteroids Battle Royale/ABR_LeaveManager.cs
@@ -7,17 +7,30 @@ using UnityEngine.UI;
 public class ABR_LeaveManager : MonoBehaviour
 {
 	[HideInInspector] public bool[] m_isReadyToLeave = null;
-	public ABR_ShipHealth[] m_shipHealth = null;
-	public GameObject[] m_LeaveUI = null;
 
-    // Start is called before the first frame update
+    [Header("External References")]
+    [Tooltip("A reference to all ships' health script.")]
+    [SerializeField]
+	private ABR_ShipHealth[] m_shipHealth = null;
+    [Tooltip("A reference to the player's leave UI.")]
+    [SerializeField]
+	private GameObject[] m_LeaveUI = null;
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
     void Awake()
     {
+        // Initialize values.
         m_isReadyToLeave = new bool[]{ false, false, false, false };
+
+        // Initialize ship healths to false.
 		foreach (var item in m_shipHealth)
 		{
 			item.m_isAlive = false;
 		}
+
+        // Initialize ship healths to alive based on active players from the main menu.
 		if (PlayerPrefs.HasKey("ABR_PlayerCount"))
 		{
 			int playercount = PlayerPrefs.GetInt("ABR_PlayerCount");
@@ -29,12 +42,17 @@ public class ABR_LeaveManager : MonoBehaviour
 		}
     }
 
+    /// <summary>
+    /// Allows players to leave from the menu.
+    /// </summary>
+    /// <param name="player">An int describing the index of which player is being affected.</param>
 	public void SetLeaveStateTrue(int player)
 	{
 		m_isReadyToLeave[player] = true;
 		m_shipHealth[player].m_isAlive = false;
 		m_LeaveUI[player].SetActive(true);
 		bool leave = true;
+
 		for (int i = 0; i < m_isReadyToLeave.Length; i++)
 		{
 			if (!m_isReadyToLeave[i] && m_shipHealth[i].m_isAlive)
@@ -44,9 +62,16 @@ public class ABR_LeaveManager : MonoBehaviour
 			}
 		}
 
-		if (leave) LeaveGame();
-
+        if (leave)
+        {
+            LeaveGame();
+        }
 	}
+
+    /// <summary>
+    /// Changes the player's UI to resume gameplay.
+    /// </summary>
+    /// <param name="player">An int describing the index of which player is being affected.</param>
 	public void SetLeaveStateFalse(int player)
 	{
 		m_isReadyToLeave[player] = false;
@@ -54,6 +79,9 @@ public class ABR_LeaveManager : MonoBehaviour
 		m_LeaveUI[player].SetActive(false);
 	}
 
+    /// <summary>
+    /// Actually leaves the games back to arcade menu.
+    /// </summary>
 	private void LeaveGame()
 	{
 		Debug.Log("End Game");
@@ -64,7 +92,7 @@ public class ABR_LeaveManager : MonoBehaviour
 	/// Returns an array containing the alive state of all player ships. 
 	/// If a player has not joined the game, it is considered dead. If a player is attempting to end the game early, they are still considered alive.
 	/// </summary>
-	/// <returns></returns>
+	/// <returns>A list of states of whether ships are alive or not.</returns>
     public bool[] GetStatesOfShips()
     {
 		bool[] isAliveStates = new bool[m_shipHealth.Length];
