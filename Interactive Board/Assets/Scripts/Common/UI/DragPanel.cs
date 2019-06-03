bc6f9baf7c7c1c752672a3053313182e3e23f187
Christopher Robinson <CRobinson@student.neumont.edu>
2019-06-03 14:19:17 -0600

    Add Dynamic Joystick and Finalize Controls

--- Interactive Board/Assets/Scripts/Common/UI/DragPanel.cs
+++ Interactive Board/Assets/Scripts/Common/UI/DragPanel.cs
@@ -4,22 +4,46 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+public delegate void PointerEventDelegate(PointerEventData data);
+
 public class DragPanel : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     public Vector2 TouchPosition { get; private set; } = Vector2.zero;
     public bool PointerDown { get; private set; }
 
+    public event PointerEventDelegate onPointerDrag;
+    public event PointerEventDelegate onPointerUp;
+    public event PointerEventDelegate onPointerDown;
+
+    public void AddPointerDragListener(PointerEventDelegate pointerEventDelegate)
+    {
+        onPointerDrag += pointerEventDelegate;
+    }
+
+    public void AddPointerUpListener(PointerEventDelegate pointerEventDelegate)
+    {
+        onPointerUp += pointerEventDelegate;
+    }
+
+    public void AddPointerDownListener(PointerEventDelegate pointerEventDelegate)
+    {
+        onPointerDown += pointerEventDelegate;
+    }
+
     public void OnDrag(PointerEventData data)
     {
         Vector3 newPos = data.position;
-
         TouchPosition = newPos;
+
+        onPointerDrag?.Invoke(data);
     }
 
     public void OnPointerUp(PointerEventData data)
     {
         TouchPosition = Vector2.zero;
         PointerDown = false;
+
+        onPointerUp?.Invoke(data);
     }
 
 
@@ -27,5 +51,7 @@ public class DragPanel : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, 
     {
         PointerDown = true;
         OnDrag(data);
+
+        onPointerDown?.Invoke(data);
     }
 }
