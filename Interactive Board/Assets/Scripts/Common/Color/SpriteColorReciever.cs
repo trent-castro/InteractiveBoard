151a28cdeb40cd1284376087e4e1e44b78acb289
Christopher Robinson <CRobinson@student.neumont.edu>
2019-06-04 15:44:30 -0600

    colors

---
+++ Interactive Board/Assets/Scripts/Common/Color/SpriteColorReciever.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteColorReciever : ColorReciever
+{
+    public override void SetColors(List<NameAndColorPair> colors)
+    {
+        GetComponent<SpriteRenderer>().color = GetColor(colors, m_colorToRecieve, GetComponent<SpriteRenderer>().color);
+    }
+}
