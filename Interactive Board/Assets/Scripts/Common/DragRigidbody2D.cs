739932a8d2f795167e0578aa17f6a29acf5ca2ba
Unknown <nenschan@gmail.com>
2019-06-05 14:58:07 -0600

    Commenting DragRigidbody2D

--- Interactive Board/Assets/Scripts/Common/DragRigidbody2D.cs
+++ Interactive Board/Assets/Scripts/Common/DragRigidbody2D.cs
@@ -6,26 +6,51 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class DragRigidbody2D : MonoBehaviour
 {
+	/// <summary>
+	/// The springjoint used to smoothly move an object around.
+	/// </summary>
     private SpringJoint2D m_springJoint = null;
+	/// <summary>
+	/// The rigidbody the springjoint is dragging.
+	/// </summary>
+    private Rigidbody2D m_rigidbody = null;
+	[Tooltip ("The available area for touch events to be resistered.")]
     [SerializeField]
     private CompositeTouchArea m_touchArea = null;
-    [SerializeField]
+	[Tooltip("The available area that can be grabbed.")]
+	[SerializeField]
     private Collider2D m_grabbableArea = null;
-    private Rigidbody2D m_rigidbody = null;
 
-    public float m_dampingRatio = .05f;
-    public float m_frequency = 5;
+	[Tooltip("The dampening of the spring joint.")]
+	public float m_dampingRatio = .05f;
+	[Tooltip("The frequency of the SpringJoint")]
+	public float m_frequency = 5;
 
-    public float m_drag = 50;
-    public float m_angularDrag = 5;
+	[Tooltip("The drag on the connected body of the SpringJoint")]
+	public float m_drag = 50;
+	[Tooltip("The angular drag on the connected body of the SpringJoint")]
+	public float m_angularDrag = 5;
+	/// <summary>
+	/// The original drag of the pulled rigidbody that is stored to be restored after the drag.
+	/// </summary>
     private float m_oldDrag;
-    private float m_oldAngularDrag;
+	/// <summary>
+	///  The original angular drag of the pulled rigidbody that is stored to be restored after the drag.
+	/// </summary>
+	private float m_oldAngularDrag;
 
+	/// <summary>
+	/// The touch that owns this SpringJoint.
+	/// </summary>
     private TouchInfo m_touchInfo = null;
 
+	[Header("Debug Controls")]
     public TextMeshPro m_debugOutput = null;
     public string m_debugString = "";
 
+	/// <summary>
+	/// Defines whether or not this Springjoint can currently be grabbed by a touch.
+	/// </summary>
     private bool canGrab = true;
 
     private void Start()
@@ -36,6 +61,11 @@ public class DragRigidbody2D : MonoBehaviour
         MultiTouchManager.Instance.ListenForTouchesOnOverlapWithEvents(m_touchArea.OverlapPoint, CheckForLetGo);
     }
 
+	/// <summary>
+	/// Checks to see if a touchevent is a valid touch event to grab this object.
+	/// </summary>
+	/// <param name="touchInfo"></param>
+	/// <param name="eventType"></param>
     private void CheckForGrab(TouchInfo touchInfo, ETouchEventType eventType)
     {
         if (canGrab && eventType != ETouchEventType.EXIT && m_touchInfo == null && touchInfo.m_owners == 0)
@@ -48,6 +78,11 @@ public class DragRigidbody2D : MonoBehaviour
         }
     }
 
+	/// <summary>
+	/// Checks if the touchevent is exiting and releases this object from being grabbed
+	/// </summary>
+	/// <param name="touchInfo"></param>
+	/// <param name="eventType"></param>
     private void CheckForLetGo(TouchInfo touchInfo, ETouchEventType eventType)
     {
         if (eventType == ETouchEventType.EXIT && m_touchInfo != null && touchInfo.FingerId == m_touchInfo.FingerId)
@@ -59,12 +94,19 @@ public class DragRigidbody2D : MonoBehaviour
         }
     }
 
+	/// <summary>
+	/// Allows this object to be grabed after a small delay.
+	/// </summary>
+	/// <returns></returns>
     private IEnumerator SetCanGrabAfterDelay()
     {
         yield return null;
         canGrab = true;
     }
 
+	/// <summary>
+	/// Grabs this object and creates a springjoint if one does not exist.
+	/// </summary>
     private void Grab()
     {
         if (!m_springJoint)
@@ -92,6 +134,10 @@ public class DragRigidbody2D : MonoBehaviour
         OnTouchMove(m_touchInfo);
     }
 
+	/// <summary>
+	/// Cleans up touch info once the touch event controlign this object ends
+	/// </summary>
+	/// <param name="touchInfo"></param>
     private void OnTouchEnd(TouchInfo touchInfo)
     {
         if (m_springJoint.connectedBody)
@@ -107,6 +153,10 @@ public class DragRigidbody2D : MonoBehaviour
         m_touchInfo = null;
     }
 
+	/// <summary>
+	/// Moves this object according to the touch event controlling it.
+	/// </summary>
+	/// <param name="touchInfo"></param>
     private void OnTouchMove(TouchInfo touchInfo)
     {
         Debug.DrawLine(m_springJoint.transform.position, transform.position, Color.red);
@@ -115,6 +165,10 @@ public class DragRigidbody2D : MonoBehaviour
         CreateDebugString("Dragging");
     }
 
+	/// <summary>
+	/// Sets debug lines to inclue all relevant informaton
+	/// </summary>
+	/// <param name="state"></param>
     private void CreateDebugString(string state)
     {
         if (m_debugOutput != null)
